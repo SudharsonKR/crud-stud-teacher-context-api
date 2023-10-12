9b4d713ee1f2583272480630dd407ba1cff75f9b
SudharsonKR <sudharsonkr@gmail.com>
2023-10-12 15:26:29 +0530

    first commit

---
+++ src/Components/Dashbord.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Base from "../Base/base";
+
+function Dashboard(){
+    return(
+        <Base
+        title={"Students and Teachers Application"}
+        descriptions={"Students and Teachers information available"}>
+            <h3>All Students and Teachers Data you can view in this site
+            </h3>
+        </Base>
+    )
+}
+export default Dashboard;
